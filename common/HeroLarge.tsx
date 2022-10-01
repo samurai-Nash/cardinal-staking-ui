4bd1b666c34ddedbeb5e1499c8cd14aa252db91e
ZeroQ4 <dotss87@gmail.com>
2022-10-02 01:04:21 +0300

    Update HeroLarge.tsx

--- common/HeroLarge.tsx
+++ common/HeroLarge.tsx
@@ -29,9 +29,9 @@ export const HeroLarge: React.FC = () => {
         `}
       />
       {config?.imageUrl ? (
-        <div className="relative flex w-1/4 grow items-center justify-center rounded-xl">
+        <div className="relative flex w-1/4 grow items-center justify-center">
           <img
-            className={`max-h-[200px] w-auto rounded-xl ${
+            className={`max-h-[50px] w-auto ${
               config?.logoPadding && 'p-8'
             }`}
             src={config?.imageUrl}
